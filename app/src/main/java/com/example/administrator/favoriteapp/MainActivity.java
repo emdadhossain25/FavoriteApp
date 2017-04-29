16e2436e7b96a46e781c6ea847a59be62295b315
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-04-29 23:46:25 +0600

    adding scrollerview ending lesson1

--- app/src/main/java/com/example/administrator/favoriteapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/favoriteapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.administrator.favoriteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO (8) Declare a TextView variable called mToysListTextView
+    TextView mToysListTextView;
+    String[] toyNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO (10) Use findViewById to get a reference to the TextView from the layout
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+
+        // TODO (11) Use the static ToyBox.getToyNames method and store the names in a String array
+        toyNames = ToyBox.getToyNames();
+
+        // TODO (12) Loop through each toy and append the name to the TextView (add \n for spacing)
+
+        for (String toyName: toyNames){
+            mToysListTextView.append(toyName+"\n\n\n");
+        }
     }
 }

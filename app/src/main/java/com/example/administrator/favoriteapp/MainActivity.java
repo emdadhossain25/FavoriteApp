db3b4ff7d3d69c9a172f97e5e5a8137df56e7c36
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-04-30 21:17:37 +0600

    finishing lesson 2 excercise 1

--- app/src/main/java/com/example/administrator/favoriteapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/favoriteapp/MainActivity.java
@@ -2,27 +2,30 @@ package com.example.administrator.favoriteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-    // TODO (8) Declare a TextView variable called mToysListTextView
-    TextView mToysListTextView;
-    String[] toyNames;
+
+    // TODO (26) Create an EditText variable called mSearchBoxEditText
+    EditText mSearchBoxEditText;
+    // TODO (27) Create a TextView variable called mUrlDisplayTextView
+    TextView mUrlDispalyTextView;
+    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    TextView mSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // TODO (10) Use findViewById to get a reference to the TextView from the layout
-        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
 
-        // TODO (11) Use the static ToyBox.getToyNames method and store the names in a String array
-        toyNames = ToyBox.getToyNames();
 
-        // TODO (12) Loop through each toy and append the name to the TextView (add \n for spacing)
-
-        for (String toyName: toyNames){
-            mToysListTextView.append(toyName+"\n\n\n");
-        }
+        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
+        mUrlDispalyTextView = (TextView) findViewById(R.id.tv_url_display);
+        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 }
+
